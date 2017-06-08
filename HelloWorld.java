686e0213514656b8eae365515300997fd2a28ce0
rpotam <ravi-sudhakara.potam@capgemini.com>
2017-06-08 16:44:18 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+
+		System.out.println ("Hellow World from jenkins");
+	}
+
+	
+}
